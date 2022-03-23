06b18d69138e42aa150a9d2d72c99103078f60ca
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2022-03-23 15:06:26 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
+const Header = () => {
+    return (
+        <nav className='header_Navigation'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="/Home">Home</a>
+                <a href="/Order">Order</a>
+                <a href="/Shop">Shop</a>
+                <a href="/Inventory">Inventory</a>
+                <a href="/About">About</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+    const [cart, setCart] = useState([])
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart)
+    }
+
+    return (
+        <div className='shop_Container'>
+            {/* Product Aria */}
+            <div className='products_Container'>
+                {
+                    products.map(product =>
+                        <Product
+                            key={product.id}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        ></Product>)
+                }
+            </div>
+            {/* Cart Arid */}
+            <div className='card_Container'>
+                <h3>Order Summary </h3>
+                <p>Selected Items : {cart.length}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
