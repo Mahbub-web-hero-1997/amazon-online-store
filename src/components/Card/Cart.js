7d520e2082cc576deb53fb1574f748bddbcf1dc5
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2022-03-24 12:14:36 +0600

    new added

--- src/components/Card/Cart.js
+++ src/components/Card/Cart.js
@@ -2,21 +2,24 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = (props) => {
-    const { cart } = props;   
+    const { cart } = props;
     // Calculate Price 
     let total = 0;
+    let shipping = 0;
     for (const product of cart) {
         total = total + product.price;
+        shipping = shipping + product.shipping;
     }
-
+    const tax = parseFloat(total * 10 / 100);
+    const grandTotal = total + shipping + tax;
     return (
         <div className='cart'>
             <h3>Order Summary </h3>
             <p>Selected Items :{cart.length} </p>
             <p>Total-Price : ${total}</p>
-            <p>Total-Shipping : { }</p>
-            <p>Tax : { }</p>
-            <h5>Grand-Total : { }</h5>
+            <p>Total-Shipping : ${shipping}</p>
+            <p>Tax : {tax.toFixed(2)}</p>
+            <h5>Grand-Total : {grandTotal}</h5>
         </div>
     );
 };
